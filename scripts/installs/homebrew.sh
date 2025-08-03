f33bed60d1687f8fb5872b75341f5cd9baad1ef6
Noctu'Apps <noctuaapps@gmail.com>
2025-08-03 21:06:53 +1100

    Adding brew programs

--- scripts/installs/homebrew.sh
+++ scripts/installs/homebrew.sh
@@ -6,3 +6,13 @@ curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
 if ! grep -q "source $HOME/.local/share/macforge/zsh/brew" "$HOME/.zshrc"; then
   echo "source $HOME/.local/share/macforge/zsh/brew" >>"$HOME/.zshrc"
 fi
+
+source "$HOME/.zshrc"
+
+for formula in $(cat "$HOME/.local/share/macforge/scripts/installs/formulas.txt"); do
+  brew install "$formula"
+done
+
+for cask in $(cat "$HOME/.local/share/macforge/scripts/installs/casks.txt"); do
+  brew install --cask "$cask"
+done
