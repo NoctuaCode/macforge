a14f218264d677293e781792459912d502d6fa2b
Noctu'Apps <noctuaapps@gmail.com>
2025-08-04 22:24:33 +1100

    feat: adding bubbletea tutorial

--- cmd/tui/main.go
+++ cmd/tui/main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	tea "github.com/NoctuaCode/macforge/packages/bubbletea"
 )
 
 func main() {
-
+	p := tea.InitBubbleTea()
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
 }
